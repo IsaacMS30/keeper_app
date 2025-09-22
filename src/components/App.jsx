f72afad5fca5751cdbe0e0715eed11d970e36374
isaacMS300 <102270561+IsaacMS30@users.noreply.github.com>
2025-09-22 16:48:34 -0600

    Added Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 //import { useState } from 'react'
 import '../styles/App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
 
   return (
     <div className='App'>
     <Header/>
+    <Footer/>
     </div>
   )
 }
