b2e73ebd2ec60b43af32ff410b1dee02d11d6c5d
isaacMS300 <102270561+IsaacMS30@users.noreply.github.com>
2025-11-07 14:23:34 -0600

    Added API to show random user on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../styles/Footer.css'
 
 function Footer() {
-  const year = new Date().getFullYear();
+  const [user, setUser] = useState("")
+  const [city, setCity] = useState("")
+
+  const getRandomUser = async () => {
+    await fetch("https://randomuser.me/api/", {
+      method: "GET"
+    }).then(res => res.json())
+    .then(res => {
+      const name = res.results[0].name
+      const location = `${res.results[0].location.state}, ${res.results[0].location.country}`
+      const userString = Object.values(name).join(" ")
+      setUser(userString)
+      setCity(location)
+    })
+  }
+
+  useEffect(() => {
+    getRandomUser()
+  }, [])
+
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>{user ? `Today you are ${user} from ${city}` : "Loading your new identity"}</p>
     </footer>
   );
 }
